a5317cdc380bde043bc2816b321598248d27c83a
RidGamerQ <118125622+RidGamerQ@users.noreply.github.com>
2022-12-03 20:37:46 +0200

    Add files via upload

--- зад.4.cpp
+++ зад.4.cpp
@@ -1,13 +1,15 @@
 ﻿#include <iostream>
 #include <math.h>
 using namespace std;
-
+double fun(int a, int b)
+{return cbrt(abs(a - b));
+}
 int main()
 {
     double y, a, b, x, t; x = 1.7;
     cout << "\n Vvdite t: "; cin >> t;
     b = sqrt(pow(x, 2) + pow(t, 2));
     a = log10(x);
-    y = cbrt(abs(a - b));
+    y = fun(a, b);
     cout << "\n y= " << y << endl;
 }
